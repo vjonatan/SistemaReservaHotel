b70e84b9944ccdbe358bfd274789170815058817
Jonatan Vera <jonatan.e.vera@gmail.com>
2019-05-25 09:38:15 -0300

    Version inicial, con pantalla de Habitaciones.

---
+++ src/Logica/Conexion.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JVera
+ */
+public class Conexion {
+    public String db = "bdreserva";
+    public String url = "jdbc://mysql://localhost" + db;
+    public String user = "root";
+    public String pass = "";
+
+    public Conexion() {
+    }
+    
+    public Connection conectar (){
+        Connection link = null;
+        
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver");
+            link = DriverManager.getConnection(this.url, this.user, this.pass);
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+            
+        }
+        return link;
+    }
+    
+    
+}
